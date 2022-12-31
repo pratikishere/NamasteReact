53fd15787349fb257efd0acfe68b93bfe7cc8767
pratikishere <pbpatel1990@yahoo.com>
2022-12-31 13:15:53 +0530

    Move code to App.js file

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading1 = React.createElement(
+  "h1",
+  {
+    key: "heading1",
+  },
+  "Heading 1"
+);
+const heading2 = React.createElement(
+  "h1",
+  {
+    key: "heading2",
+  },
+  "Heading 2"
+);
+const container = React.createElement("div", {}, [heading1, heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
