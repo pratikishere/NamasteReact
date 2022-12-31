ebf4f8e71360d1c844586255cbdeaf3bd629b985
pratikishere <pbpatel1990@yahoo.com>
2022-12-31 13:19:45 +0530

    Add id to div tag and add new css to apply bg to div

--- App.js
+++ App.js
@@ -12,6 +12,12 @@ const heading2 = React.createElement(
   },
   "Heading 2"
 );
-const container = React.createElement("div", {}, [heading1, heading2]);
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
