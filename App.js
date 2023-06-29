2bdb737d56d9335d0ef9176d91323a8a4266c36b
pratikishere <pbpatel1990@yahoo.com>
2023-06-29 19:38:58 +0530

    Define functional component and render it

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React element
 const heading = <h1 key="heading">Heading using JSX</h1>;
 
+const HeadingComponent = () => {
+  return (
+    <div>
+      {heading}
+      <h1>Namste React functional component</h1>
+      <HeadingComponentShort />
+      {HeadingComponentShort()}
+    </div>
+  );
+};
+
+const HeadingComponentShort = () => (
+  <div>
+    <h1>Short functional component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
